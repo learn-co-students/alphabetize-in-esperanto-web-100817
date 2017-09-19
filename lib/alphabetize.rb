af2f1232475bd50fa6a1949ef5b94ada18b3b591
Dick Ward <dick@thedickward.com>
2017-09-19 00:07:54 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+  new_array = arr.sort_by do |word_or_phrase|
+    word_or_phrase.chars.collect do |letter|
+        esperanto_alphabet.index(letter)
+      end
+  end
+   new_array
 end
+
+#alphabetize(["bonan matenon", "ĉu vi parolas esperanton", "mi amas vin", "pacon"])
