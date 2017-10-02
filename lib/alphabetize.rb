7cf5e67244fa2ee2aac70e3bf15142b243f0f700
Brian Boisvert <b.m.boisvert@gmail.com>
2017-10-02 16:28:59 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,6 @@
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  #code here
+  arr.sort_by { |a| a.split("").collect{ |char| ESPERANTO_ALPHABET.index(char) } }
 end
